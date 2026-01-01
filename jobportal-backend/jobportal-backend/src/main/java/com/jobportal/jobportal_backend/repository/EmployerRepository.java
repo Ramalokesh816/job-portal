3c1b72025b2401acaef160b76db51dd438e8d141
lokesh <jramalokesh04@gmail.com>
2026-01-01 17:31:57 +0530

    Frontend

--- jobportal-backend/jobportal-backend/src/main/java/com/jobportal/jobportal_backend/repository/EmployerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobportal.jobportal_backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.jobportal.jobportal_backend.model.Employer;
-
-public interface EmployerRepository
-        extends MongoRepository<Employer, String> {
-}
--- jobportal-backend/jobportal-backend/src/main/java/com/jobportal/jobportal_backend/repository/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jobportal.jobportal_backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.jobportal.jobportal_backend.model.Job;
-
-public interface JobRepository extends MongoRepository<Job, String> {
-}
