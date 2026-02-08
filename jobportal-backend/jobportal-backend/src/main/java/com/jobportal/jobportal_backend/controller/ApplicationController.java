1a8826dc936d318cf1ed5ef8e72e71e5d996877f
lokesh <jramalokesh04@gmail.com>
2026-02-08 16:07:20 +0530

    Fix CORS

--- jobportal-backend/jobportal-backend/src/main/java/com/jobportal/jobportal_backend/controller/ApplicationController.java
+++ jobportal-backend/jobportal-backend/src/main/java/com/jobportal/jobportal_backend/controller/ApplicationController.java
@@ -16,7 +16,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,7 +34,6 @@ import com.jobportal.jobportal_backend.service.EmailService;
 
 @RestController
 @RequestMapping("/api/applications")
-@CrossOrigin(origins = "*")
 public class ApplicationController {
 
     private static final Logger logger = LoggerFactory.getLogger(ApplicationController.class);
--- jobportal-backend/jobportal-backend/src/main/java/com/jobportal/jobportal_backend/controller/UserController.java
+++ jobportal-backend/jobportal-backend/src/main/java/com/jobportal/jobportal_backend/controller/UserController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +18,6 @@ import com.jobportal.jobportal_backend.security.JwtUtil;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
